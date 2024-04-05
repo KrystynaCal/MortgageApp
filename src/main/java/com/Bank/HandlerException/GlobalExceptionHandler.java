e6323bbb70b418208d258e4167a7d16da1810931
KrystynaCal <krystynacal5@gmail.com>
2024-04-05 20:27:00 +0200

    Added Exception Handler

---
+++ src/main/java/com/Bank/HandlerException/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.Bank.HandlerException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleException(IllegalArgumentException e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    }
+}
